e8f3bf9b151b9391a93b6e8e70c521023caa9b74
Terry Davis <wahyudi.roesmal@gmail.com>
2025-12-03 11:24:43 +0100

    reinig gemaakt

--- testing.py
+++ testing.py
@@ -2,6 +2,5 @@ a = "string"
 b = "jatoch"
 import re
 string = "ha: (kapot, nee, ja, oke stop.)\n jatog"
-inhoud = re.search(r'\((.*?)\)', string)
-match = inhoud.group(1)
-print(match)
+ja = string.find("(")
+print(ja)
